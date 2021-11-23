b5be03963b61dcf6813330b885fdd7f6e44a901a
Anshu Pasrija <anshu.pasrija@gmail.com>
2021-11-23 11:46:06 -0800

    reverse string

--- challenge1.js
+++ challenge1.js
@@ -1,14 +1,26 @@
-const parseMessage = (origin, message) => {
-  // Code here!
-    return (origin + ":" + " " + message);
-  // Want to see what's going on inside your function? 
-  // You can use console.log() to debug and print variables to the console below.
-  
-  // Remember to return a value!
+// const parseMessage = (origin, message) => {
+//   // Code here!
+//     return (origin + ":" + " " + message);
+//   // Want to see what's going on inside your function?
+//   // You can use console.log() to debug and print variables to the console below.
+
+//   // Remember to return a value!
+// }
+//  parseMessage("Mission Control", "Hello there!")
+
+// //  Output:
+
+// // Mission Control: Hello there!
+
+
+
+const reverse = function(string){
+let backward = "";
+for (let i = string.length - 1; i >= 0; i--) {
+  backward += string[i];
 }
- parseMessage("Mission Control", "Hello there!")
-
-//  Output:
-
-// Mission Control: Hello there!
-    
+ return backward;
+}
+process.argv.slice(2).forEach(string => {
+  console.log(reverse(string));
+});
