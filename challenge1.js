10875edc7c46d3c213e58f76a2c1791c7819bace
Anshu Pasrija <anshu.pasrija@gmail.com>
2021-11-23 10:00:35 -0800

    first commit

---
+++ challenge1.js
@@ -0,0 +1,14 @@
+const parseMessage = (origin, message) => {
+  // Code here!
+    return (origin + ":" + " " + message);
+  // Want to see what's going on inside your function? 
+  // You can use console.log() to debug and print variables to the console below.
+  
+  // Remember to return a value!
+}
+ parseMessage("Mission Control", "Hello there!")
+
+//  Output:
+
+//     Mission Control: Hello there!
+    
