89b13b280fe9cf3a828071684e0c0f123dabf8fa
Anshu Pasrija <anshu.pasrija@gmail.com>
2021-11-25 19:19:52 -0800

    challenge 4

--- challenge4.js
+++ challenge4.js
@@ -1,9 +1,32 @@
 const args = process.argv;
 let result = "";
 for (let i = 2; i < args.length; i++) {
-  console.log(args[i]);
   let ending = (args[i][0]) + "ay ";
   let newString = (args[i].slice(1)) + ending;
   result += newString;
 }
 console.log(result);
+
+// Input:
+
+//     const someToggle = {
+//       name:"toggleA",
+//       isOn:false
+//     }
+    
+// Output:
+
+//     const someToggle = {
+//       name:"toggleA",
+//       isOn:true
+//     }
+
+const switchToggle = (toggle) => {
+  // Code here
+     toggle.isOn = ! toggle.isOn
+     console.log(toggle.isOn);
+      return toggle
+  
+  // Remember to return a value!
+}
+
